bb0184d3b05bfe78cbe88f44f701cfaa8483ff67
Norbu Tsering <ntsering91@gmail.com>
2017-07-05 14:29:02 -0400

    Now solve for indexes of hex tile

--- hexy/storage.py
+++ hexy/storage.py
@@ -15,6 +15,12 @@ Key Rules:
 - a and b can be negative, so it is okay if the key is written as
   '-a,b' or 'a,-b' or '-a,-b'
 """
+import numpy as np
+from util import DIR, cube_to_axial
+
+# The bases of the axial coordinate system
+__bases_mat = np.array([cube_to_axial(DIR.SE), cube_to_axial(DIR.E)],
+                       dtype=int)
 
 
 class HexExistsError(Exception):
@@ -27,19 +33,30 @@ class IncorrectCoordinatesError(Exception):
         super(IncorrectCoordinatesError, self).__init__(message)
 
 
+def make_key_from_coordinate(coordinate):
+    if len(coordinate) != 2:
+        raise IncorrectCoordinatesError(str(coordinate) + " has incorrect length.")
+
+    return str(coordinate[0]) + ',' + str(coordinate[1])
+
+
+def solve_for_indexes(hexes):
+    """
+    We want to solve for the coefficients in the linear combos.
+    :param hexes: The hexes whose indexes we want to solve for.
+                  nx2, n=number of hexes
+    :return: indexes of `hexes`
+    """
+    return np.linalg.solve(__bases_mat, hexes.T).T
+
+
 class HexMap(dict):
     def __init__(self):
         super(HexMap, self).__init__()
 
     def __setitem__(self, coordinate, hex_object):
-        key = self.make_key_from_coordinate(coordinate)
+        key = make_key_from_coordinate(coordinate)
         if key in dict.keys():
             raise HexExistsError("key " + key + " already exists.")
 
         self[key] = hex_object
-
-    def make_key_from_coordinate(self, coordinate):
-        if len(coordinate) != 2:
-            raise IncorrectCoordinatesError(str(coordinate) + " has incorrect length.")
-
-        return str(coordinate[0]) + ',' + str(coordinate[1])
