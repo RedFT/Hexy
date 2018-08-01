a000c7b2a487f7590c0e3c6da76672ef8d2ec328
Norbu Tsering <norbu.tsering.cs@gmail.com>
2018-07-31 22:59:29 -0700

    Modified conversion tests so that pytest can find and run them

--- test/test_conversions.py
+++ test/test_conversions.py
@@ -6,36 +6,39 @@ import hexy as hx
 
 hm = hx.HexMap()
 
-
-def same_mat(A, B):
-    if np.unique(A == B) == [True]:
-        return True
-    return False
-
-
 radius = 10
 coords = hx.get_spiral(np.array((0, 0, 0)), 1, 10)
 
-# check axial <-> cube conversions work
-axial_coords = hx.cube_to_axial(coords)
-cube_coords = hx.axial_to_cube(axial_coords)
+def test_axial_to_cube_conversion():
+    axial_coords = hx.cube_to_axial(coords)
+    cube_coords = hx.axial_to_cube(axial_coords)
 
-print same_mat(coords, cube_coords)
+    assert np.array_equal(coords, cube_coords)
 
-# check axial <-> pixel conversions work
-pixel_coords = hx.axial_to_pixel(axial_coords, radius)
-pixel_to_axial_coords = hx.pixel_to_axial(pixel_coords, radius)
+def test_axial_to_pixel_conversion():
+    axial_coords = hx.cube_to_axial(coords)
+    pixel_coords = hx.axial_to_pixel(axial_coords, radius)
+    pixel_to_axial_coords = hx.pixel_to_axial(pixel_coords, radius)
 
-print same_mat(axial_coords, pixel_to_axial_coords)
+    assert np.array_equal(axial_coords, pixel_to_axial_coords)
 
-# check cube <-> pixel conversions work
-pixel_coords = hx.cube_to_pixel(cube_coords, radius)
-pixel_to_cube_coords = hx.pixel_to_cube(pixel_coords, radius)
-pixel_to_cube_to_axial_coords = hx.cube_to_axial(pixel_to_cube_coords)
+def test_cube_to_pixel_conversion():
+    axial_coords = hx.cube_to_axial(coords)
+    cube_coords = hx.axial_to_cube(axial_coords)
+    pixel_coords = hx.cube_to_pixel(cube_coords, radius)
+    pixel_to_cube_coords = hx.pixel_to_cube(pixel_coords, radius)
+    pixel_to_cube_to_axial_coords = hx.cube_to_axial(pixel_to_cube_coords)
 
-print same_mat(cube_coords, pixel_to_cube_coords)
+    assert np.array_equal(cube_coords, pixel_to_cube_coords)
 
-# check that we can correctly retrieve hexes after conversions
-hm[axial_coords] = coords
-retrieved = hm[pixel_to_cube_to_axial_coords]
-print same_mat(retrieved, cube_coords)
+def test_the_converted_coords_and_dataset_coords_retrieve_the_same_data():
+    axial_coords = hx.cube_to_axial(coords)
+    cube_coords = hx.axial_to_cube(axial_coords)
+    pixel_coords = hx.cube_to_pixel(cube_coords, radius)
+    pixel_to_cube_coords = hx.pixel_to_cube(pixel_coords, radius)
+    pixel_to_cube_to_axial_coords = hx.cube_to_axial(pixel_to_cube_coords)
+
+    # check that we can correctly retrieve hexes after conversions
+    hm[axial_coords] = coords
+    retrieved = hm[pixel_to_cube_to_axial_coords]
+    assert np.array_equal(retrieved, cube_coords)
