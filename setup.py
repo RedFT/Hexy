1902da4370065d36c57420bd4a9858cc7309a2bd
Norbu Tsering <norbu.tsering.cs@gmail.com>
2019-04-04 03:17:05 +0000

    bumped minor version.

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from distutils.core import setup
 setup(
   name = 'Hexy',         
   packages = ['hexy'],   
-  version = '1.1',      
+  version = '1.2',      
   license='MIT',        
   description = 'A library that makes working with a hexagonal lattice easier.',   
   author = 'Norbu Tsering',                   
   author_email = 'norbu.tsering.cs@gmail.com',      
   url = 'https://github.com/redft/hexy',   
-  download_url = 'https://github.com/RedFT/Hexy/archive/1.1.tar.gz',    
+  download_url = 'https://github.com/RedFT/Hexy/archive/1.2.tar.gz',    
   keywords = ['hexy', 'coordinate', 'hexagon', 'hexagonal'],
   install_requires=[            
     'atomicwrites==1.1.5',
