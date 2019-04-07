e45fb4d04bf1b432c87e945faf3e68eac1e3b001
Norbu Tsering <norbu.tsering.cs@gmail.com>
2019-04-07 03:25:00 +0000

    Added test for cube_round

---
+++ test/test_rounding.py
@@ -0,0 +1,15 @@
+import numpy as np
+import hexy as hx
+
+def test_cube_round():
+    test_coords = np.array([
+        [1.1, -1.4, 0.3],
+        [3.3, 2.3, -5.4],
+        ]);
+
+    expected_coords = np.array([
+        [1, -1, 0],
+        [3,  2, -5],
+        ]);
+
+    assert(np.array_equal(hx.cube_round(test_coords), expected_coords))
