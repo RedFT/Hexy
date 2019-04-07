be003b52f67fa12a6200ca7e0c50be1097beb517
Norbu Tsering <norbu.tsering.cs@gmail.com>
2019-04-07 03:35:30 +0000

    axial_round test

--- test/test_rounding.py
+++ test/test_rounding.py
@@ -9,7 +9,20 @@ def test_cube_round():
 
     expected_coords = np.array([
         [1, -1, 0],
-        [3,  2, -5],
+        [3, 2, -5],
         ]);
 
     assert(np.array_equal(hx.cube_round(test_coords), expected_coords))
+
+def test_axial_round():
+    test_coords = np.array([
+        [1.1, -1.4],
+        [3.3, 2.3],
+        ]);
+
+    expected_coords = np.array([
+        [1, -1],
+        [3, 2],
+        ]);
+
+    assert(np.array_equal(hx.axial_round(test_coords), expected_coords))
