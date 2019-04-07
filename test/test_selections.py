d3d9524cca20eda69f40e6d4bf0c416f703bea1f
Norbu Tsering <norbu.tsering.cs@gmail.com>
2019-04-07 05:13:22 +0000

    Added test for get_spiral

--- test/test_selections.py
+++ test/test_selections.py
@@ -49,6 +49,58 @@ def test_get_ring():
         hx.get_ring([0, 0, 0], 0),
         [[0, 0, 0]]))
 
+def test_get_spiral():
+    # For radius of 3
+    expected = []
+
+    r = 2
+    expected.append((r - 0) * hx.W + 0 * hx.NW)
+    expected.append((r - 1) * hx.W + 1 * hx.NW)
+
+    expected.append((r - 0) * hx.NW + 0 * hx.NE)
+    expected.append((r - 1) * hx.NW + 1 * hx.NE)
+
+    expected.append((r - 0) * hx.NE + 0 * hx.E)
+    expected.append((r - 1) * hx.NE + 1 * hx.E)
+
+    expected.append((r - 0) * hx.E + 0 * hx.SE)
+    expected.append((r - 1) * hx.E + 1 * hx.SE)
+
+    expected.append((r - 0) * hx.SE + 0 * hx.SW)
+    expected.append((r - 1) * hx.SE + 1 * hx.SW)
+
+    expected.append((r - 0) * hx.SW + 0 * hx.W)
+    expected.append((r - 1) * hx.SW + 1 * hx.W)
+
+    r += 1
+    expected.append((r - 0) * hx.W + 0 * hx.NW)
+    expected.append((r - 1) * hx.W + 1 * hx.NW)
+    expected.append((r - 2) * hx.W + 2 * hx.NW)
+
+    expected.append((r - 0) * hx.NW + 0 * hx.NE)
+    expected.append((r - 1) * hx.NW + 1 * hx.NE)
+    expected.append((r - 2) * hx.NW + 2 * hx.NE)
+
+    expected.append((r - 0) * hx.NE + 0 * hx.E)
+    expected.append((r - 1) * hx.NE + 1 * hx.E)
+    expected.append((r - 2) * hx.NE + 2 * hx.E)
+
+    expected.append((r - 0) * hx.E + 0 * hx.SE)
+    expected.append((r - 1) * hx.E + 1 * hx.SE)
+    expected.append((r - 2) * hx.E + 2 * hx.SE)
+
+    expected.append((r - 0) * hx.SE + 0 * hx.SW)
+    expected.append((r - 1) * hx.SE + 1 * hx.SW)
+    expected.append((r - 2) * hx.SE + 2 * hx.SW)
+
+    expected.append((r - 0) * hx.SW + 0 * hx.W)
+    expected.append((r - 1) * hx.SW + 1 * hx.W)
+    expected.append((r - 2) * hx.SW + 2 * hx.W)
+
+    assert(np.array_equal(
+        hx.get_spiral([0, 0, 0], 2, 3),
+        np.array(expected)))
+
 def test_get_disk():
     # For radius of 3
     expected = []
