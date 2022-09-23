f3c98699b3b6211dd799539086f89ae522da18d3
Norbu Tsering <norbu.tsering.cs@gmail.com>
2022-09-23 15:47:31 -0700

    Add additional selection tests

--- test/test_selections.py
+++ test/test_selections.py
@@ -1,165 +1,113 @@
 import numpy as np
 import hexy as hx
+import pprint
 
-from pprint import pprint
 
-def test_get_ring():
-    # The way get_ring works is for a radius r,
-    # Grab two adjacent "directions" d1, d2 and find all linear combos a * d1 + b * d2
-    # such that a and b are positive integers and a + b = r, 
-    # in the implementatin we omit a * d1 + r * d2 because if this is included, we'll 
-    # get duplicates coordinates.
+# The way get_ring works is:
+#   For a radius r, grab two adjacent "directions" d1, d2 and find all linear
+#   combinations a * d1 + b * d2 such that a and b are positive integers and
+#   a + b = r.
+#
+# In the implementation, we omit a * d1 + r * d2 because if this is included, we'll
+# get duplicates coordinates.
+def test_get_ring_with_positive_radius():
+    r=3
+    expected = _get_linear_combinations(hx.W, hx.NW, r) + \
+               _get_linear_combinations(hx.NW, hx.NE, r) + \
+               _get_linear_combinations(hx.NE, hx.E, r) + \
+               _get_linear_combinations(hx.E, hx.SE, r) + \
+               _get_linear_combinations(hx.SE, hx.SW, r) + \
+               _get_linear_combinations(hx.SW, hx.W, r)
 
-    # For radius of 3
-    r = 3
-    expected = []
-    expected.append((r - 0) * hx.W + 0 * hx.NW)
-    expected.append((r - 1) * hx.W + 1 * hx.NW)
-    expected.append((r - 2) * hx.W + 2 * hx.NW)
+    _assert_np_array_equal(hx.get_ring([0, 0, 0], r), expected)
 
-    expected.append((r - 0) * hx.NW + 0 * hx.NE)
-    expected.append((r - 1) * hx.NW + 1 * hx.NE)
-    expected.append((r - 2) * hx.NW + 2 * hx.NE)
 
-    expected.append((r - 0) * hx.NE + 0 * hx.E)
-    expected.append((r - 1) * hx.NE + 1 * hx.E)
-    expected.append((r - 2) * hx.NE + 2 * hx.E)
+def test_get_ring_with_negative_radius():
+    _assert_np_array_equal(hx.get_ring([0, 0, 0], -1), [])
 
-    expected.append((r - 0) * hx.E + 0 * hx.SE)
-    expected.append((r - 1) * hx.E + 1 * hx.SE)
-    expected.append((r - 2) * hx.E + 2 * hx.SE)
 
-    expected.append((r - 0) * hx.SE + 0 * hx.SW)
-    expected.append((r - 1) * hx.SE + 1 * hx.SW)
-    expected.append((r - 2) * hx.SE + 2 * hx.SW)
+def test_get_ring_with_zero_radius():
+    _assert_np_array_equal(hx.get_ring([0, 0, 0], 0), [[0, 0, 0]])
 
-    expected.append((r - 0) * hx.SW + 0 * hx.W)
-    expected.append((r - 1) * hx.SW + 1 * hx.W)
-    expected.append((r - 2) * hx.SW + 2 * hx.W)
-
-    assert(np.array_equal(
-        hx.get_ring([0, 0, 0], r),
-        expected))
-
-    assert(np.array_equal(
-        hx.get_ring([0, 0, 0], -1),
-        []))
-
-    assert(np.array_equal(
-        hx.get_ring([0, 0, 0], 0),
-        [[0, 0, 0]]))
 
 def test_get_spiral():
-    # For radius of 3
+    start_radius = 2
+    end_radius = 4
     expected = []
+    for r in range(start_radius, end_radius + 1):
+        expected += _get_linear_combinations(hx.W, hx.NW, r) + \
+                    _get_linear_combinations(hx.NW, hx.NE, r) + \
+                    _get_linear_combinations(hx.NE, hx.E, r) + \
+                    _get_linear_combinations(hx.E, hx.SE, r) + \
+                    _get_linear_combinations(hx.SE, hx.SW, r) + \
+                    _get_linear_combinations(hx.SW, hx.W, r)
 
-    r = 2
-    expected.append((r - 0) * hx.W + 0 * hx.NW)
-    expected.append((r - 1) * hx.W + 1 * hx.NW)
+    actual_array = hx.get_spiral([0, 0, 0], start_radius, end_radius)
+    expected_array = np.array(expected)
+    _assert_np_array_equal(actual_array, expected_array)
 
-    expected.append((r - 0) * hx.NW + 0 * hx.NE)
-    expected.append((r - 1) * hx.NW + 1 * hx.NE)
 
-    expected.append((r - 0) * hx.NE + 0 * hx.E)
-    expected.append((r - 1) * hx.NE + 1 * hx.E)
+def test_get_spiral():
+    start_radius = 2
+    end_radius = 4
+    expected = []
+    for r in range(start_radius, end_radius + 1):
+        expected += _get_linear_combinations(hx.W, hx.NW, r) + \
+                    _get_linear_combinations(hx.NW, hx.NE, r) + \
+                    _get_linear_combinations(hx.NE, hx.E, r) + \
+                    _get_linear_combinations(hx.E, hx.SE, r) + \
+                    _get_linear_combinations(hx.SE, hx.SW, r) + \
+                    _get_linear_combinations(hx.SW, hx.W, r)
 
-    expected.append((r - 0) * hx.E + 0 * hx.SE)
-    expected.append((r - 1) * hx.E + 1 * hx.SE)
+    actual_array = hx.get_spiral([0, 0, 0], start_radius, end_radius)
+    expected_array = np.array(expected)
+    _assert_np_array_equal(actual_array, expected_array)
 
-    expected.append((r - 0) * hx.SE + 0 * hx.SW)
-    expected.append((r - 1) * hx.SE + 1 * hx.SW)
-
-    expected.append((r - 0) * hx.SW + 0 * hx.W)
-    expected.append((r - 1) * hx.SW + 1 * hx.W)
-
-    r += 1
-    expected.append((r - 0) * hx.W + 0 * hx.NW)
-    expected.append((r - 1) * hx.W + 1 * hx.NW)
-    expected.append((r - 2) * hx.W + 2 * hx.NW)
-
-    expected.append((r - 0) * hx.NW + 0 * hx.NE)
-    expected.append((r - 1) * hx.NW + 1 * hx.NE)
-    expected.append((r - 2) * hx.NW + 2 * hx.NE)
-
-    expected.append((r - 0) * hx.NE + 0 * hx.E)
-    expected.append((r - 1) * hx.NE + 1 * hx.E)
-    expected.append((r - 2) * hx.NE + 2 * hx.E)
-
-    expected.append((r - 0) * hx.E + 0 * hx.SE)
-    expected.append((r - 1) * hx.E + 1 * hx.SE)
-    expected.append((r - 2) * hx.E + 2 * hx.SE)
-
-    expected.append((r - 0) * hx.SE + 0 * hx.SW)
-    expected.append((r - 1) * hx.SE + 1 * hx.SW)
-    expected.append((r - 2) * hx.SE + 2 * hx.SW)
-
-    expected.append((r - 0) * hx.SW + 0 * hx.W)
-    expected.append((r - 1) * hx.SW + 1 * hx.W)
-    expected.append((r - 2) * hx.SW + 2 * hx.W)
-
-    assert(np.array_equal(
-        hx.get_spiral([0, 0, 0], 2, 3),
-        np.array(expected)))
 
 def test_get_disk():
-    # For radius of 3
-    expected = []
+    expected = [[0, 0, 0]]
+    radius=3
+    for r in range(radius+1):
+        expected += _get_linear_combinations(hx.W, hx.NW, r) + \
+                    _get_linear_combinations(hx.NW, hx.NE, r) + \
+                    _get_linear_combinations(hx.NE, hx.E, r) + \
+                    _get_linear_combinations(hx.E, hx.SE, r) + \
+                    _get_linear_combinations(hx.SE, hx.SW, r) + \
+                    _get_linear_combinations(hx.SW, hx.W, r)
 
-    r = 0
-    expected.append(hx.W * r)
+    actual_array = hx.get_disk([0, 0, 0], radius)
+    expected_array = np.array(expected)
+    _assert_np_array_equal(actual_array, expected_array)
 
-    r += 1
-    expected.append((r - 0) * hx.W + 0 * hx.NW)
-    expected.append((r - 0) * hx.NW + 0 * hx.NE)
-    expected.append((r - 0) * hx.NE + 0 * hx.E)
-    expected.append((r - 0) * hx.E + 0 * hx.SE)
-    expected.append((r - 0) * hx.SE + 0 * hx.SW)
-    expected.append((r - 0) * hx.SW + 0 * hx.W)
 
-    r += 1
-    expected.append((r - 0) * hx.W + 0 * hx.NW)
-    expected.append((r - 1) * hx.W + 1 * hx.NW)
+def test_get_disk_not_at_origin():
+    center = hx.SE
+    expected = [center]
+    radius=3
+    for r in range(radius+1):
+        expected += _get_linear_combinations(hx.W, hx.NW, r, center) + \
+                    _get_linear_combinations(hx.NW, hx.NE, r, center) + \
+                    _get_linear_combinations(hx.NE, hx.E, r, center) + \
+                    _get_linear_combinations(hx.E, hx.SE, r, center) + \
+                    _get_linear_combinations(hx.SE, hx.SW, r, center) + \
+                    _get_linear_combinations(hx.SW, hx.W, r, center)
 
-    expected.append((r - 0) * hx.NW + 0 * hx.NE)
-    expected.append((r - 1) * hx.NW + 1 * hx.NE)
+    actual_array = hx.get_disk(center, radius)
+    expected_array = np.array(expected)
+    _assert_np_array_equal(actual_array, expected_array)
 
-    expected.append((r - 0) * hx.NE + 0 * hx.E)
-    expected.append((r - 1) * hx.NE + 1 * hx.E)
 
-    expected.append((r - 0) * hx.E + 0 * hx.SE)
-    expected.append((r - 1) * hx.E + 1 * hx.SE)
+def _assert_np_array_equal(actual, expected):
+    if not np.array_equal(actual, expected):
+        msg = (
+                f"Expected with length {len(expected)}: \n"
+                f"{pprint.pformat(expected)}\n"
+                f"But got with length {len(actual)}: \n"
+                f"{pprint.pformat(actual)}\n"
+        )
+        raise AssertionError(msg)
 
-    expected.append((r - 0) * hx.SE + 0 * hx.SW)
-    expected.append((r - 1) * hx.SE + 1 * hx.SW)
 
-    expected.append((r - 0) * hx.SW + 0 * hx.W)
-    expected.append((r - 1) * hx.SW + 1 * hx.W)
+def _get_linear_combinations(d1, d2, r, center = np.array([0, 0, 0])):
+    return [(r - i) * d1 + i * d2 + center for i in range(r)]
 
-    r += 1
-    expected.append((r - 0) * hx.W + 0 * hx.NW)
-    expected.append((r - 1) * hx.W + 1 * hx.NW)
-    expected.append((r - 2) * hx.W + 2 * hx.NW)
-
-    expected.append((r - 0) * hx.NW + 0 * hx.NE)
-    expected.append((r - 1) * hx.NW + 1 * hx.NE)
-    expected.append((r - 2) * hx.NW + 2 * hx.NE)
-
-    expected.append((r - 0) * hx.NE + 0 * hx.E)
-    expected.append((r - 1) * hx.NE + 1 * hx.E)
-    expected.append((r - 2) * hx.NE + 2 * hx.E)
-
-    expected.append((r - 0) * hx.E + 0 * hx.SE)
-    expected.append((r - 1) * hx.E + 1 * hx.SE)
-    expected.append((r - 2) * hx.E + 2 * hx.SE)
-
-    expected.append((r - 0) * hx.SE + 0 * hx.SW)
-    expected.append((r - 1) * hx.SE + 1 * hx.SW)
-    expected.append((r - 2) * hx.SE + 2 * hx.SW)
-
-    expected.append((r - 0) * hx.SW + 0 * hx.W)
-    expected.append((r - 1) * hx.SW + 1 * hx.W)
-    expected.append((r - 2) * hx.SW + 2 * hx.W)
-
-    assert(np.array_equal(
-        hx.get_disk([0, 0, 0], r),
-        np.array(expected)))
