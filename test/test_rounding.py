dd3a0e1e6c85b27db9e0dcd271e85c96269c93ec
xm <contact@techartserv.com>
2023-01-17 21:06:34 +0100

    semicolons wound pythonystas very badly, really, they do... save the pythonistas

--- test/test_rounding.py
+++ test/test_rounding.py
@@ -5,12 +5,12 @@ def test_cube_round():
     test_coords = np.array([
         [1.1, -1.4, 0.3],
         [3.3, 2.3, -5.4],
-        ]);
+        ])
 
     expected_coords = np.array([
         [1, -1, 0],
         [3, 2, -5],
-        ]);
+        ])
 
     assert(np.array_equal(hx.cube_round(test_coords), expected_coords))
 
@@ -18,11 +18,11 @@ def test_axial_round():
     test_coords = np.array([
         [1.1, -1.4],
         [3.3, 2.3],
-        ]);
+        ])
 
     expected_coords = np.array([
         [1, -1],
         [3, 2],
-        ]);
+        ])
 
     assert(np.array_equal(hx.axial_round(test_coords), expected_coords))
