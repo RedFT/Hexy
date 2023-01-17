c7b52c557222b6f36daf3f479bc235e68c20a9b4
xm <contact@techartserv.com>
2023-01-17 20:58:05 +0100

    semicolons wound pythonystas very badly, save the pythonistas

--- test/test_hex_line.py
+++ test/test_hex_line.py
@@ -12,10 +12,10 @@ def test_get_hex_line():
     start = np.array([-3, 3, 0])
     end = np.array([1, 1, -2])
     print(hx.get_hex_line(start, end))
-    print(expected);
+    print(expected)
     assert(np.array_equal(
         hx.get_hex_line(start, end),
-        expected));
+        expected))
 
 if __name__ == "__main__":
     test_get_hex_line()
